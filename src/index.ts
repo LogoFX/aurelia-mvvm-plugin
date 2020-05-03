159871e6ddd85962ec015c6c6637ad96e7b96aa5
David Kossoglyad <david@logoui.co.il>
2020-05-03 14:24:29 +0300

    feat(logofx): add last working logofx files

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import {FrameworkConfiguration} from 'aurelia-framework';
 import {PLATFORM} from 'aurelia-pal';
 
+export * from './logofx';
+
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
     PLATFORM.moduleName('./elements/hello-world'),
-    PLATFORM.moduleName('./attributes/color')
+    PLATFORM.moduleName('./attributes/color'),
+    PLATFORM.moduleName('./logofx/index')
   ]);
+
 }
