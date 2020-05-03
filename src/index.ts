78e8f506cb755a5a0d05dd32484e5a7247785d0c
David Kossoglyad <david@logoui.co.il>
2020-05-03 13:40:02 +0300

    fix(sample): fix sample in dev-app

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {PLATFORM} from 'aurelia-pal';
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
-    PLATFORM.moduleName('./elements/hello-world')
+    PLATFORM.moduleName('./elements/hello-world'),
+    PLATFORM.moduleName('./attributes/color')
   ]);
 }
