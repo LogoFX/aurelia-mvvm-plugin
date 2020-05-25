3825ff26a493326b66f3d465ff57871a7c698ad7
David Kossoglyad <david@logoui.co.il>
2020-05-25 14:39:28 +0300

    feat(dev-app): setup dev-app based on "app-ux-showcase"
    Maybe it wasn't good idea...

--- dev-app/src/app.ts
+++ dev-app/src/app.ts
@@ -1,11 +1,50 @@
-import { arrays } from '@logofx/aurelia-mvvm-plugin';
+import { StyleEngine } from '@aurelia-ux/core';
+import { autoinject } from 'aurelia-dependency-injection';
+import { RouterConfiguration, Router } from 'aurelia-router';
+import { routes } from './routes';
+import * as themes from './themes.json';
 
 
+@autoinject
 export class App {
+  private router: Router;
+  private theme = localStorage.getItem('theme');
 
-  private ar: number[] = [1,2,3];
- 
-  public message: string = arrays.indexOf(this.ar, 4).toString();
+  constructor(private styleEngine: StyleEngine) {
+    this.styleEngine.applyThemeGroup(this.theme == 'dark' ? (<any>themes).dark : (<any>themes).light);
+  }
 
+  configureRouter(config: RouterConfiguration, router: Router) {
+    this.router = router;
 
+    config.options.pushState = true;
+    config.options.root = '/';
+
+    config.map(routes);
+  }
+
+  public toggleTheme() {
+    this.theme = this.theme === 'light' ? 'dark' : 'light';
+
+    localStorage.setItem('theme', this.theme);
+    this.styleEngine.applyThemeGroup(this.theme == 'dark' ? (<any>themes).dark : (<any>themes).light);
+  }
+}
+
+export class CategoriesValueConverter {
+  toView(navModels) {
+    let categories = new Map();
+
+    for (let model of navModels) {
+      let routes = categories.get(model.settings.category);
+
+      if (!routes) {
+        categories.set(model.settings.category, routes = []);
+      }
+
+      routes.push(model);
+    }
+
+    return categories;
+  }
 }
